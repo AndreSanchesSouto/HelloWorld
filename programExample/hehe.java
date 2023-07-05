d1ae9b0a3173c25060bcc05040f93764bc3f9f1c
André Sanches Souto <andresanchessouto@gmail.com>
2023-07-04 21:51:34 -0300

    adicaoEscrita

--- programExample/hehe.java
+++ programExample/hehe.java
@@ -3,7 +3,8 @@ package programExample;
 public class hehe{
 
     public static void main(String[] args){
-        System.out.print("Hello World!");
+        System.out.println("Hello World!");
+        System.out.print("Estou aos poucos entendendo");
     }
 
 } 
